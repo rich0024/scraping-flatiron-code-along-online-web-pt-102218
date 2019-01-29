bae3945652982097deca81b8c43375ee5c3c76e3
Richard Yance <yance0022@gmail.com>
2019-01-29 18:04:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,17 +13,3 @@ class Scraper
   end
 
 end
-
-Scraper.new.get_page
-    def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
-
-end
